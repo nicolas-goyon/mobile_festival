90abf1a4543ececa8c5dc702198966d092a65690
macmobile <nico_goyon@orange.fr>
2024-03-27 15:07:10 +0100

    modification DTO

--- FestivalMobile/FestivalMobile/Models/AttributionDTO.swift
+++ FestivalMobile/FestivalMobile/Models/AttributionDTO.swift
@@ -7,9 +7,16 @@
 
 import Foundation
 
-class AttributionDTO {
-    let id_benevole : String // UUID
-    let id_zone : String // UUID
-    let est_referent : Bool
-    let id_creneau : String // UUID
+class AttributionDTO: Codable{
+    let id_benevole : String
+    let id_zone : String
+    let est_referent: Bool
+    let id_creneau : String
+    
+    init(id_benevole: String, id_zone: String, est_referent: Bool, id_creneau: String){
+        self.id_benevole = id_benevole
+        self.id_zone = id_zone
+        self.est_referent = est_referent
+        self.id_creneau = id_creneau
+    }
 }
